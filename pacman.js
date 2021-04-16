7a47e79da973f603e95c69fe2c6ef3f1a388edc3
dee912 <devantespaine@gmail.com>
2021-04-16 22:05:54 +0100

    PM tunnel and rand food

--- pacman.js
+++ pacman.js
@@ -10,12 +10,14 @@ const cells = []
 let pacManPos = 19
 let lives = 3
 
-let ghosts = {
+const ghosts = {
   clydePos: 152,
   blinkyPos: 153,
   pinkyPos: 170,
   inkyPos: 171,
 }
+
+let food = 20
 
 // create cells
 
@@ -26,6 +28,7 @@ for (let i = 0; i < width ** 2; i++) {
   div.style.width = `${100 / width}%`
   div.style.height = `${100 / width}%`
   cells.push(div)
+  
 }
 
 // add classes from css to pacman, ghosts and food
@@ -35,23 +38,25 @@ cells[ghosts.clydePos].classList.add('clyde')
 cells[ghosts.blinkyPos].classList.add('blinky')
 cells[ghosts.pinkyPos].classList.add('pinky')
 cells[ghosts.inkyPos].classList.add('inky')
+cells[food].classList.add('food')
+// cells[food].classList.add('food')
 
 console.log(ghosts[1])
 
 //create a functions to remove pacman if ghostPos === pacManPos
 
-function pacmanReset() {
+function pacManReset() {
   if (pacManPos === ghosts.clydePos || 
     pacManPos === ghosts.blinkyPos || 
     pacManPos === ghosts.pinkyPos || 
     pacManPos === ghosts.inkyPos) {
-
     cells[pacManPos].classList.remove('pacman')
     pacManPos = 19
     lives--
     cells[pacManPos].classList.add('pacman')
   }
 }
+
 
 // add event listener to start button to begin game
 // assign controls to pacman and change his position 
@@ -60,17 +65,27 @@ function pacmanReset() {
 
 startButton.addEventListener('click', () => {
 // console.log(ghosts.clydePos)
+
+  setInterval(() => {
+    if (food < 325) {
+      food++
+      cells[food].classList.add('food')
+    }
+  }, 1000)
+
   setInterval(() => {
     if (ghosts.clydePos < ((width ** 2) - (width * 2))) {
       cells[ghosts.clydePos].classList.remove('clyde')
-      ghosts.clydePos += width
+      ghosts.clydePos++
       cells[ghosts.clydePos].classList.add('clyde')
-    }
-    pacmanReset()
+    } 
+    pacManReset()
   }, 1000)
 
   document.addEventListener('keydown', (e) => {
-  
+    
+    const randomIndex = Math.floor(Math.random() * width ** 2 - 1)
+    //pacman contorls
     const key = e.key
     if (key === 's' && pacManPos < ((width ** 2) - (width * 2))) {
       cells[pacManPos].classList.remove('pacman')
@@ -80,16 +95,34 @@ startButton.addEventListener('click', () => {
       cells[pacManPos].classList.remove('pacman')
       pacManPos -= width
       cells[pacManPos].classList.add('pacman')
-    } else if (key === 'd' && !((pacManPos + 2) % width === 0)) {
+    } else if (key === 'd' && !((pacManPos + 2) % width === 0) || key === 'd' && pacManPos === 160) {
       cells[pacManPos].classList.remove('pacman')
       pacManPos++
       cells[pacManPos].classList.add('pacman')
-    } else if (key === 'a' && !((pacManPos - 1) % width === 0)) {
+    } else if (key === 'a' && !((pacManPos - 1) % width === 0) || key === 'a' && pacManPos === 145) {
       cells[pacManPos].classList.remove('pacman')
       pacManPos--
       cells[pacManPos].classList.add('pacman')
     }
-    pacmanReset()
+    //tunnel at centre of the map
+    if (pacManPos === 162) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos = 144
+      cells[pacManPos].classList.add('pacman')
+    } else if (pacManPos === 143) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos = 161
+      cells[pacManPos].classList.add('pacman')
+    }
+    //food rules
+
+    if (pacManPos === food) {
+      cells[food].classList.remove('food')
+      food = randomIndex
+      cells[food].classList.add('food')
+    }
+
+    pacManReset()
   })
 
 })
