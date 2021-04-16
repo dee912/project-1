c53d99f83c958e30e68b1f21421174607a5a65c0
dee912 <devantespaine@gmail.com>
2021-04-16 14:03:35 +0100

    PM ghost kills pacman

--- pacman.js
+++ pacman.js
@@ -6,8 +6,16 @@ const grid = document.querySelector('.grid')
 const startButton = document.querySelector('#start-button')
 const width = 18
 const cells = []
+
 let pacManPos = 19
 let lives = 3
+
+let ghosts = {
+  clydePos: 152,
+  blinkyPos: 153,
+  pinkyPos: 170,
+  inkyPos: 171,
+}
 
 // create cells
 
@@ -23,33 +31,68 @@ for (let i = 0; i < width ** 2; i++) {
 // add classes from css to pacman, ghosts and food
 
 cells[pacManPos].classList.add('pacman')
+cells[ghosts.clydePos].classList.add('clyde')
+cells[ghosts.blinkyPos].classList.add('blinky')
+cells[ghosts.pinkyPos].classList.add('pinky')
+cells[ghosts.inkyPos].classList.add('inky')
+
+console.log(ghosts[1])
+
+//create a functions to remove pacman if ghostPos === pacManPos
+
+function pacmanReset() {
+  if (pacManPos === ghosts.clydePos || 
+    pacManPos === ghosts.blinkyPos || 
+    pacManPos === ghosts.pinkyPos || 
+    pacManPos === ghosts.inkyPos) {
+
+    cells[pacManPos].classList.remove('pacman')
+    pacManPos = 19
+    lives--
+    cells[pacManPos].classList.add('pacman')
+  }
+}
 
 // add event listener to start button to begin game
 // assign controls to pacman and change his position 
 //! movable area is 16 x 16 accounting for boarder
 //! use 'keydown' so pacman can move continuously 
 
-// startButton.addEventListener('click', () => {
+startButton.addEventListener('click', () => {
+// console.log(ghosts.clydePos)
+  setInterval(() => {
+    if (ghosts.clydePos < ((width ** 2) - (width * 2))) {
+      cells[ghosts.clydePos].classList.remove('clyde')
+      ghosts.clydePos += width
+      cells[ghosts.clydePos].classList.add('clyde')
+    }
+    pacmanReset()
+  }, 1000)
 
-document.addEventListener('keydown', (e) => {
-  // console.log(e.key)
-  const key = e.key
-  if (key === 's' && pacManPos < ((width ** 2) - (width * 2))) {
-    cells[pacManPos].classList.remove('pacman')
-    pacManPos += width
-    cells[pacManPos].classList.add('pacman')
-  } else if (key === 'w' && pacManPos > width * 2 - 1) {
-    cells[pacManPos].classList.remove('pacman')
-    pacManPos -= width
-    cells[pacManPos].classList.add('pacman')
-  } else if (key === 'd' && !((pacManPos + 2) % width === 0)) {
-    cells[pacManPos].classList.remove('pacman')
-    pacManPos++
-    cells[pacManPos].classList.add('pacman')
-  } else if (key === 'a' && !((pacManPos - 1) % width === 0)) {
-    cells[pacManPos].classList.remove('pacman')
-    pacManPos--
-    cells[pacManPos].classList.add('pacman')
-  }
+  document.addEventListener('keydown', (e) => {
+  
+    const key = e.key
+    if (key === 's' && pacManPos < ((width ** 2) - (width * 2))) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos += width
+      cells[pacManPos].classList.add('pacman')
+    } else if (key === 'w' && pacManPos > width * 2 - 1) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos -= width
+      cells[pacManPos].classList.add('pacman')
+    } else if (key === 'd' && !((pacManPos + 2) % width === 0)) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos++
+      cells[pacManPos].classList.add('pacman')
+    } else if (key === 'a' && !((pacManPos - 1) % width === 0)) {
+      cells[pacManPos].classList.remove('pacman')
+      pacManPos--
+      cells[pacManPos].classList.add('pacman')
+    }
+    pacmanReset()
+  })
+
 })
-// })
+
+
+
